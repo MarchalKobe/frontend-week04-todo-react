c612aeec60e336f730fa0f735d4f782416fa83ce
Kobe Marchal <kobe.marchal@student.howest.be>
2021-03-09 11:17:05 +0100

    Finishing touches

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/objects/container.css';
+import '../style/objects/row.css';
+
+const AppFooter = () => {
+    return (
+        <div className="o-row">
+            <footer className="o-container">
+                <p>&copy; M.K. - powered by 🕊</p>
+            </footer>
+        </div>
+    );
+};
+
+export default AppFooter;
