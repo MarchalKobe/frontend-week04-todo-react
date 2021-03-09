70aee71f9b0bc4566167d5dd1465e4f66d7d68f2
Kobe Marchal <kobe.marchal@student.howest.be>
2021-03-09 11:21:33 +0100

    Finishing touches

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-// import reportWebVitals from './reportWebVitals';
->>>>>>> 0.0.2
 
 ReactDOM.render(
   <React.StrictMode>
