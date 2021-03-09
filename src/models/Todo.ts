f90590fd8cc96f8e302097a656fcc1ea2ae57a65
Kobe Marchal <kobe.marchal@student.howest.be>
2021-03-09 11:42:04 +0100

    Update files

---
+++ src/models/Todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+    id?: string; // ? = optionele parameter
+    title: string;
+    category: string;
+}
--- src/screens/Todos.tsx
+++ src/screens/Todos.tsx
@@ -16,7 +16,7 @@ const Todos = () => {
                 <Container>
                     <div className="c-add-todo">
                         <button className="c-add-todo-button">
-                            <svg className="c-add-todo-button__icon" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="arcs">
+                            <svg className="c-add-todo-button__icon" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round">
                                 <line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line>
                             </svg>
                         </button>
